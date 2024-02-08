cd6a79dfbbbb9cf2bd32098f0af85e12ad2dad0c
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2024-02-08 17:48:45 +0900

    Time: 88 ms (74.66%), Space: 21 MB (86.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,12 +1,12 @@
 from collections import defaultdict
 from typing import List
 
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
         dict = defaultdict(int)
-        for num in nums :
+        for num in nums:
             dict[num] += 1
 
-        answer = sorted(dict.keys(),key = lambda x : dict[x] ,reverse=True)[:k]
+        answer= sorted(dict.keys(), key=lambda x: dict[x], reverse=True)[:k]
         return answer
