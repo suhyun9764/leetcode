ea977e9ddf025ecd381f6943eee4772d1c0b80c8
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2024-02-13 17:25:09 +0900

    Time: 49 ms (86.96%), Space: 16.7 MB (58.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,14 +1,23 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # s를 돌면서 알파벳 하나씩 딕셔너리에 저장
         dict = {}
+        max = 0
         start = 0
-        max_length = 0
         for i in range(len(s)):
-            if s[i] in dict:
-                start = max(start,dict[s[i]] + 1)
+            current_length = 0
+            if s[i] in dict and start <= dict[s[i]]:
+                current_length = dict[s[i]] - start +1
+                start = dict[s[i]]+1
+                dict[s[i]] = i
+            else :
+                dict[s[i]] = i
+                current_length = i - start+1
 
-            dict[s[i]] = i
-            max_length = max(max_length,i - start + 1)
+            if max < current_length:
+                max = current_length
 
-        return max_length
+        return max
+
+s = "pwwkew"
+sol = Solution()
+print(sol.lengthOfLongestSubstring(s))
