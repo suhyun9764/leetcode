00394a12a3bf455e79b3ac18cfaa1477010c9f36
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2024-02-13 14:50:45 +0900

    Time: 1002 ms (14.47%), Space: 31.8 MB (33.14%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,14 +1,21 @@
+from collections import deque
 from typing import List
+
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer = [0]*len(temperatures)
-        stack = []
-        for i in range(len(temperatures)):
-            while stack and temperatures[i]>temperatures[stack[-1]]:
-                pre_idx = stack.pop()
-                answer[pre_idx] = i - pre_idx
+        deq = deque(enumerate(temperatures))
+        stack_buffer = []
+        answer = [0] * len(temperatures)
+        while len(deq) > 0:
+            curr = deq.popleft()
+            while stack_buffer and stack_buffer[-1][1] < curr[1]:
+                targetDay = stack_buffer.pop()
+                answer[targetDay[0]] = curr[0] - targetDay[0]
 
-            stack.append(i)
-
+            stack_buffer.append(curr)
         return answer
+
+sol = Solution()
+temperatures = [73,74,75,71,69,72,76,73]
+print(sol.dailyTemperatures(temperatures))
