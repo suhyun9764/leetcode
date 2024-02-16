e128bf7a91d982463799d851dadcb9d0056b2869
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2024-02-16 10:44:38 +0900

    Time: 234 ms (79.05%), Space: 18.7 MB (98.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,32 +1,29 @@
-from typing import List
-
-
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslands(self, grid):
         dx = [0,0,1,-1]
         dy = [1,-1,0,0]
 
-        m = len(grid)
-        n = len(grid[0])
-
-        cnt =0
-        def dfs(r,c) :
-            if r < 0 or r >= m or c < 0 or c >=n or grid[r][c] != '1':
-                return
-
-            grid[r][c] = '0'
-            for i in range(4):
-                dfs(r+dx[i], c+dy[i])
-
-            return
-        
-        for i in range(m):
-            for j in range(n):
+        n = len(grid)
+        m = len(grid[0])
+        cnt = 0
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j] == '0':
                     continue
 
-                dfs(i,j)
-                cnt+=1
+                cnt +=1
+                que = deque()
+                que.append((i,j))
+                while que:
+                    y,x = que.popleft()
+                    for k in range(4):
+                        ny = y+dy[k]
+                        nx = x+dx[k]
+
+                        if ny <0  or ny >=n or nx <0 or nx >= m or grid[ny][nx] == '0':
+                            continue
+
+                        grid[ny][nx] = '0'
+                        que.append((ny,nx))
 
         return cnt
-
